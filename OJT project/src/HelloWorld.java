138789c0f4ea353637d6b4b10295c0b72a983272
John Mari D. Hinolan <hinolanjohn@yahoo.com>
2014-04-10 13:24:25 -0700

    initial commit

---
+++ OJT project/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+public class HelloWorld {public void main(String args){
+	//timer now = new timer();
+	System.out.print("");
+	
+	
+	
+	
+	
+} 
+}
+
