12cba124547a170d8cd2cfe25dde4de104043e8d
John Mari D. Hinolan <hinolanjohn@yahoo.com>
2014-04-10 13:38:36 -0700

    First branch commit

--- OJT project/src/HelloWorld.java
+++ OJT project/src/HelloWorld.java
@@ -1,14 +1,26 @@
 import java.util.*;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
-public class HelloWorld {public void main(String args){
+public class HelloWorld {
+	public static void main(String [] args){
 	//timer now = new timer();
 	System.out.print("");
 	
 	
-	
+	Calendar cal = Calendar.getInstance();
+	cal.getTime();
+	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	System.out.println( sdf.format(cal.getTime()) );
 	
 	
 } 
 }
 
+class timer {
+	
+	
+	
+	
+	
+	
+}
